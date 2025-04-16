25c0696006b52788f3b1767295afea6da4e1d660
AKSHAY M <akshaym0507@outlook.com>
2025-04-16 12:08:37 +0530

    Update main.py for CORS Middleware

--- main.py
+++ main.py
@@ -14,10 +14,19 @@ from dotenv import load_dotenv
 import os
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 load_dotenv()
 
 memory = MemorySaver()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 llm = AzureChatOpenAI(
     api_key=os.getenv("AZURE_OPENAI_API_KEY"),
